0e768d39d22ab83a3f0c51a7f4f5aa548f766240
최준화 <choijunhwa072@gmail.com>
2026-04-30 19:16:49 +0900

    Update 1-3.c

---
+++ code/1-3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+char *my_strcat(char *dst, const char *src) {
+    char *ptr = dst;
+    while(*ptr != '\0') {
+        ptr++;
+    }
+    while(*src != '\0') {
+        *ptr = *src;
+        *ptr++;
+        *src++;
+    }
+    return dst;
+}
+
+int main() {
+    char str1[10] = "Hello";
+    char str2[] = " World";
+
+    printf("%s\n", str1);
+    my_strcat(str1, str2);
+    printf("%s\n", str1);
+
+    return 0;
+}
